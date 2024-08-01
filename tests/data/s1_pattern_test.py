8d712e17b55d00c7ee399e96249942a0a4fd2144
May <punnaruck@users.noreply.github.com>
2024-08-01 22:28:21 +0700

    Step1 Test

---
+++ tests/data/s1_pattern_test.py
@@ -0,0 +1,31 @@
+from testcases.mc4_testcases import (
+    DOCUMENT_REMOVE_TEST_CASES,
+    CLEAN_TEXT_TEST_CASES,
+    CLEAN_DATASET_TEST_CASES,
+)
+
+from openthaigpt_pretraining_data.internet.s1_pattern.preprocess_pattern import (
+    clean_with_remove_document,
+    clean_text,
+    clean_dataset,
+)
+
+from utils_test import compare_dataset
+
+import copy
+
+
+def test_document_remove():
+    for test_case in DOCUMENT_REMOVE_TEST_CASES:
+        assert clean_with_remove_document(test_case["doc"]) == test_case["remove"]
+
+
+def test_clean_text():
+    for test_case in CLEAN_TEXT_TEST_CASES:
+        assert clean_text(test_case["doc"]) == test_case["new_doc"]
+
+
+def test_clean_dataset():
+    for test_case in CLEAN_DATASET_TEST_CASES:
+        test_case = copy.deepcopy(test_case)
+        compare_dataset(clean_dataset(test_case["dataset"]), test_case["new_dataset"])
