7d2834675a7fdf807d070ec46974aaf897b2fd2b
May <punnaruck@users.noreply.github.com>
2024-08-28 00:06:17 +0700

    Add anonymization

--- setup.py
+++ setup.py
@@ -8,5 +8,5 @@ setup(
     install_requires=[
         'numpy==1.24.3', 'pandas==2.0.3', 'tqdm', 'datasets', 'datasketch==1.6.5', 'nlpo3==1.3.0',
         'pytest', 'jsonlines', 'kenlm', 'scipy==1.10.1', 'sentencepiece==0.2.0', 'scikit-learn==1.2.2', 'gdown==2.3.1',
-        "pythainlp>=4.0.0"],
+        "pythainlp>=4.0.0", "python-crfsuite==0.9.10"],
 )
