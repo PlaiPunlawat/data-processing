fd959f7e9f47c2a989544ea44098b8db92b2ffdd
May <punnaruck@users.noreply.github.com>
2024-08-27 22:52:20 +0700

    Add pythainlp lib

--- setup.py
+++ setup.py
@@ -7,5 +7,6 @@ setup(
     packages=find_packages("src"),
     install_requires=[
         'numpy==1.24.3', 'pandas==2.0.3', 'tqdm', 'datasets', 'datasketch==1.6.5', 'nlpo3==1.3.0',
-        'pytest', 'jsonlines', 'kenlm', 'scipy==1.10.1', 'sentencepiece==0.2.0', 'scikit-learn==1.2.2', 'gdown==2.3.1'],
+        'pytest', 'jsonlines', 'kenlm', 'scipy==1.10.1', 'sentencepiece==0.2.0', 'scikit-learn==1.2.2', 'gdown==2.3.1',
+        "pythainlp>=4.0.0"],
 )
